9cc5a200aa4e09e8967fb52a52b6eb929ec1ebf5
Curtis <curtissalcedo@gmail.com>
2023-05-11 13:39:24 -0600

    react to domain

--- src/components/Job/InkCoffee.jsx
+++ src/components/Job/InkCoffee.jsx
@@ -1,15 +1,17 @@
+import "./Job.css"
+
 export default function InkCoffee() {
   return (
     <>
+    <div className="jobCard">
+
     <h3>ink! Coffee Company, Denver, CO | Director of Retail Operations&nbsp;&nbsp;<small>06/2015 - 10/2022</small></h3>
-      <ul>
-        <li>Led and managed up to 16 retail store locations with operational ownership responsibilities, ensuring the delivery of exceptional customer service and achieving business targets.</li>
-        <li>Implemented modern technologies to maintain and monitor inventory levels, resulting in increased efficiency and cost savings.</li>
-        <li>Designed and implemented unique standard operating procedures (SOPs) for each location, resulting in improved performance metrics and increased profitability.</li>
-        <li>Developed and led a team of 50+ members, including managers and associates, fostering a collaborative and inclusive work culture.</li>
-        <li>Assisted in the successful launch of new store locations from the ground up.</li>
-      </ul>
-      
+        <p>Led and managed up to 16 retail store locations with operational ownership responsibilities, ensuring the delivery of exceptional customer service and achieving business targets.</p>
+        <p>Implemented modern technologies to maintain and monitor inventory levels, resulting in increased efficiency and cost savings.</p>
+        <p>Designed and implemented unique standard operating procedures (SOPs) for each location, resulting in improved performance metrics and increased profitability.</p>
+        <p>Developed and led a team of 50+ members, including managers and associates, fostering a collaborative and inclusive work culture.</p>
+        <p>Assisted in the successful launch of new store locations from the ground up.</p>
+    </div>
   </>
   )
 }
--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -1,11 +1,17 @@
 import InkCoffee from '../../components/Job/InkCoffee';
 import Adt from '../../components/Job/Adt';
+import "./Resume.css"
+
 
 export default function Resume() {
   return (
     <>
-    <InkCoffee />
-    <Adt />
+    <div className="jobContainer">
+      <InkCoffee />
+    </div>
+    <div className="jobContainer">
+      <Adt />
+    </div>
     </>
   )
 }
