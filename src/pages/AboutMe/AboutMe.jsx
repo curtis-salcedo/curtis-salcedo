b0bbcb9a5655b2fa5ec5aaba16cb83123c72b7c6
Curtis <curtissalcedo@gmail.com>
2023-11-27 08:32:25 -0700

    quick edit

--- src/pages/AboutMe/AboutMe.jsx
+++ src/pages/AboutMe/AboutMe.jsx
@@ -35,7 +35,7 @@ export default function AboutMe() {
       <div className="AboutMeTitle">About Me</div>
       <div className="AboutMeContent">
         <div className="AboutMeDescription">
-          <p>I am passionate about technology, innovation, and teamwork. With a background in operations management and expertise in both web and software development, I bring a versatile skill set to every project. My strengths include identifying improvement opportunities, implementing creative solutions, and utilizing cutting-edge technologies to enhance efficiency and performance. I am dedicated to delivering transformative solutions across various areas. I thrive on collaboration and enjoy leveraging technology to drive positive change and contribute meaningfully to others' needs..</p>
+          <p>I am passionate about technology, innovation, and teamwork. With a background in operations management and expertise in both web and software development, I bring a versatile skill set to every project. My strengths include identifying improvement opportunities, implementing creative solutions, and utilizing cutting-edge technologies to enhance efficiency and performance. I am dedicated to delivering transformative solutions across various areas. I thrive on collaboration and enjoy leveraging technology to drive positive change and contribute meaningfully to others' needs.</p>
         </div>
         <div className="AboutMeButtons">
           <button className="PortfolioButton" onClick={handleViewWorks}>Works</button>
